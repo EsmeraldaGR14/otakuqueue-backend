7b17fb0b37dab01b437d8f815297a82ba9696b15
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air.lan>
2023-08-17 00:12:41 -0400

    updated schema for my psql live

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ CREATE DATABASE otakuqueue_dev;
 
 \c otakuqueue_dev;
 
+DROP TABLE IF EXISTS anime_list;
 -- purpose of this table is to store all animes so that the user can create or select their desired anime
 CREATE TABLE anime_list (
     id SERIAL PRIMARY KEY,
@@ -14,11 +15,14 @@ CREATE TABLE anime_list (
     rating DECIMAL(3, 1)
 );
 
+DROP TABLE IF EXISTS users;
+-- the purpose of this table is so that there are users, this allows for a more personlised website
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL
 );
 
+DROP TABLE IF EXISTS user_watchlist;
 -- the purpose of this table is so that the user is able to store the animes they have selected
 CREATE TABLE user_watchlist (
     id SERIAL PRIMARY KEY,
@@ -29,5 +33,4 @@ CREATE TABLE user_watchlist (
     is_favorite BOOLEAN
 );
 
--- the purpose of this table is so that there are users, this allows for a more personlised website
 
